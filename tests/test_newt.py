f2aabefb83a62ca4f85f84f1753b17265c404d86
Scott <dscamiss@gmail.com>
2025-04-22 15:23:52 -0700

    Add test for learning rate calculation

--- tests/test_newt.py
+++ tests/test_newt.py
@@ -95,6 +95,7 @@ def test_refresh_param_cache_unexpected(affine_model: nn.Module) -> None:
 
 def test_get_param_update(affine_model: nn.Module) -> None:
     """Test `_get_param_update()` behavior."""
+    # Note the use of `lr=1.0` here
     optimizer = torch.optim.SGD(affine_model.parameters(), lr=1.0)
     newt = Newt(optimizer, None, cache_updates=True)
 
@@ -115,6 +116,7 @@ def test_get_param_update(affine_model: nn.Module) -> None:
 
 def test_compute_inner_product(affine_model: nn.Module) -> None:
     """Test `_compute_inner_product()` behavior."""
+    # Note the use of `lr=1.0` here
     optimizer = torch.optim.SGD(affine_model.parameters(), lr=1.0)
     newt = Newt(optimizer, None)
 
@@ -137,3 +139,34 @@ def test_compute_inner_product(affine_model: nn.Module) -> None:
     # Check actual versus expected
     err_str = "Error in inner product"
     assert torch.allclose(inner_product, expected_inner_product), err_str
+
+
+def test_compute_lr(affine_model: nn.Module) -> None:
+    """Test `_compute_lr()` behavior."""
+    # Note the use of `lr=1.0` here
+    optimizer = torch.optim.SGD(affine_model.parameters(), lr=1.0)
+    newt = Newt(optimizer, None, gamma=1.0)
+
+    # Force parameter updates
+    weight_update = torch.randn_like(affine_model.weight)
+    bias_update = torch.randn_like(affine_model.bias)
+    affine_model.weight.data -= weight_update
+    affine_model.bias.data -= bias_update
+
+    # Force "next loss" gradients
+    affine_model.weight.grad = weight_update
+    affine_model.bias.grad = bias_update
+
+    # Force loss and "next loss"
+    newt._curr_loss = 2.0
+    newt._next_loss = 1.0
+
+    # Compute actual and expected learning rates
+    lr = newt._compute_lr()
+    inner_product = newt._compute_inner_product()
+    expected_lr_num = inner_product
+    expected_lr_den = 2.0 * (1.0 - inner_product)
+    expected_lr = 1.0 + (expected_lr_num / (newt._epsilon + expected_lr_den))
+
+    # Check actual versus expected
+    assert torch.allclose(lr, expected_lr), "Error in learning rate"
