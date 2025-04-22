21f6370f266555bd7582563d6a758fe18f0d6f83
Scott <dscamiss@gmail.com>
2025-04-22 15:14:40 -0700

    Separate learning rate calculation

--- src/newt/newt.py
+++ src/newt/newt.py
@@ -93,6 +93,20 @@ class Newt(LRScheduler):
         return inner_product
 
     @jaxtyped(typechecker=typechecker)
+    def _compute_lr(self) -> Num[Tensor, ""]:
+        """
+        Compute next learning rate.
+
+        Returns:
+            next_lr: Next learning rate as a scalar tensor.
+        """
+        inner_product = self._compute_inner_product()
+        curr_lr = self._optimizer.param_groups[0]["lr"]
+        next_lr_num = (curr_lr ** 2.0) * inner_product  # fmt: skip
+        next_lr_den = 2.0 * (self._curr_loss - self._next_loss - (curr_lr * inner_product))
+        return curr_lr * (1.0 + self._gamma * (next_lr_num / (self._epsilon + next_lr_den)))
+
+    @jaxtyped(typechecker=typechecker)
     def _get_param_update(self, param: nn.Parameter) -> Num[Tensor, "..."]:
         param_group = self._optimizer.param_groups[0]
         curr_lr = param_group["lr"]
@@ -130,18 +144,12 @@ class Newt(LRScheduler):
         if self._step_count == 1:
             return [self._optimizer.param_groups[0]["lr"]]
 
-        # Compute inner product term
-        inner_product = self._compute_inner_product()
+        # Update learning rate
+        # TODO: Handle tensor LR
+        lr = self._compute_lr()
 
         # Refresh cached parameters
         self._refresh_param_cache()
 
-        # Update learning rate
-        # TODO: Handle tensor LR
-        curr_lr = self._optimizer.param_groups[0]["lr"]
-        next_lr_num = (curr_lr ** 2.0) * inner_product  # fmt: skip
-        next_lr_den = 2.0 * (self._curr_loss - self._next_loss - (curr_lr * inner_product))
-        next_lr = curr_lr * (1.0 + self._gamma * (next_lr_num / (self._epsilon + next_lr_den)))
-
-        # Return new learning rate
-        return [next_lr]
+        # Return updated learning rate
+        return [lr]
